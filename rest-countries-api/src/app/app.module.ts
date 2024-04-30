8f5ed5afc287957ad6fd0aca4de126d09084541b
Tim <this-is-not-my-real-email@test.com>
2024-05-01 00:49:25 +0200

    add home component and routing

--- rest-countries-api/src/app/app.module.ts
+++ rest-countries-api/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { CommonModule } from '@angular/common'
-import { RouterOutlet, provideRouter } from '@angular/router'
-import { routes } from './app.routes'
 import { BrowserModule } from '@angular/platform-browser'
 import { LayoutModule } from './layout/layout.module'
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  providers: [provideRouter(routes)],
-  imports: [CommonModule, RouterOutlet, BrowserModule, LayoutModule],
+  imports: [CommonModule, BrowserModule, LayoutModule],
 })
 export class AppModule {}
--- rest-countries-api/src/app/layout/layout.module.ts
+++ rest-countries-api/src/app/layout/layout.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser'
 import { LayoutComponent } from './layout.component'
 import { TopNavComponent } from './components/top-nav/top-nav.component'
+import { RouterModule } from '@angular/router'
+import { routes } from './layout.routes'
 
 @NgModule({
   declarations: [LayoutComponent, TopNavComponent],
   exports: [LayoutComponent],
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
 })
 export class LayoutModule {}
