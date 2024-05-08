dd11a8dbebddc9b69f086cbb37d698435994f2c9
Tim <this-is-not-my-real-email@test.com>
2024-05-08 23:10:05 +0200

    added home component with module and search and filter component

--- rest-countries-api/src/app/home/home.module.ts
+++ rest-countries-api/src/app/home/home.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home.component'
+import { SearchComponent } from './components/search/search.component'
+import { ReactiveFormsModule } from '@angular/forms';
+import { FilterComponent } from './components/filter/filter.component'
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, SearchComponent, FilterComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [HomeComponent, SearchComponent],
 })
 export class HomeModule {}
--- rest-countries-api/src/app/layout/layout.module.ts
+++ rest-countries-api/src/app/layout/layout.module.ts
@@ -5,10 +5,16 @@ import { LayoutComponent } from './layout.component'
 import { TopNavComponent } from './components/top-nav/top-nav.component'
 import { RouterModule } from '@angular/router'
 import { routes } from './layout.routes'
+import { HomeModule } from '../home/home.module'
 
 @NgModule({
   declarations: [LayoutComponent, TopNavComponent],
   exports: [LayoutComponent],
-  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HomeModule,
+  ],
 })
 export class LayoutModule {}
